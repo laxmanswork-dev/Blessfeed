d0bf29e95c12d0662424023fe1ec263523fbee2e
Laxman Shanmugam <laxmanswork.dev@gmail.com>
2026-02-08 01:15:24 +0530

    Fix Login.css import case

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./login.css";
+import "./Login.css";
 
 export default function Login() {
   const navigate = useNavigate();
